f29d94122636be5b4a3e2c0154a4392aa67e4df6
AndriiAvramenk <72814303+AndriiAvramenk@users.noreply.github.com>
2024-05-02 18:38:38 +0300

    Update

--- js/3.js
+++ js/3.js
@@ -3,4 +3,4 @@ let a = Math.floor(prompt("Enter your number: "));
 if (isNaN(a))
     console.error("This value isn't integer");
 else
-    a%2 == 1 ? alert(`${a} is odd`) : alert(`${a} is even`);
+    a%2 === 1 ? alert(`${a} is odd`) : alert(`${a} is even`);
--- js/4.js
+++ js/4.js
@@ -3,4 +3,4 @@ let year = Math.floor(prompt("Enter your year: "));
 if (isNaN(year))
     console.error("This value isn't integer");
 else
-    ((year % 100 == 0 && year % 400 != 0) || year % 4 != 0) ? alert(`${year} year isn't leap`) : alert(`${year} year is leap`);
+    ((year % 100 === 0 && year % 400 !== 0) || year % 4 !== 0) ? alert(`${year} year isn't leap`) : alert(`${year} year is leap`);
